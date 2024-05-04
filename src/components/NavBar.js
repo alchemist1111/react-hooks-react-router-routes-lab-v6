360b5492cc3aa2a0b8a443e843b8291ea7a021b7
alchemist1111 <emoyoeugine@gmail.com>
2024-05-04 14:25:28 +0300

    adds changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,27 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
+      <NavLink
+        to="/"
+        /* add styling to Navlink */
+        className="nav-link"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/directors"
+        /* add styling to Navlink */
+        className="nav-link"
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        /* add styling to Navlink */
+        className="nav-link"
+      >
+        Actors
+      </NavLink>
     
     </nav>
     );
